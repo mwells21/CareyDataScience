4b21b142fab1e83d4ad878b10ce6a82fbc05a796
mwells21 <wellsmike19@gmail.com>
2020-08-25 03:15:42 -0400

    Update covid.R

--- bookcamp/R/covid.R
+++ bookcamp/R/covid.R
@@ -15,15 +15,10 @@ p<-ggplot(data=csse_covid_19_daily_reports_us, aes(x= reorder(Province_State, -D
 # Horizontal bar plot
 p + coord_flip()
 
-p
 
 library(ggalt) # devtools::install_github("hrbrmstr/ggalt")
 library(hrbrthemes) # devtools::install_github("hrbrmstr/hrbrthemes")
 library(tidyverse)
-
-nord <- read_palette("nord.ase")
-
-show_palette(nord)
 
 nord = c("#8FBCBB",
 "#88C0D0",
@@ -46,15 +41,13 @@ world <- world[world$region != "Antarctica", ]
 usa = world[world$region=="USA"&world$long < 0 & world$long > -130,]
 
 
-
+# MAP of US - Deaths By State
 ggplot() +
   geom_cartogram(
     data = usa, map = usa,
     aes(x = long, y = lat, map_id = region),
     color = nord[[13]], fill = nord[[10]], size = 0.125
   ) +
-  #coord_proj("+proj=wintri") +
-  #scale_size_area(name = "Node count", max_size = 20, labels = scales::comma) +
   labs(
     x = NULL, y = NULL,
     title = "Covid Cases USA",
@@ -62,10 +55,9 @@ ggplot() +
     caption = "Johns Hopkins University"
   ) +
   geom_point(
-    data = csse_covid_19_daily_reports_us[(csse_covid_19_daily_reports_us$Long_ < 0 & csse_covid_19_daily_reports_us$Long_ > -130 & !is.na(csse_covid_19_daily_reports_us$Long_)),], aes(Long_, Lat, size = Deaths), fill = nord[[7]],
+    data = csse_covid_19_daily_reports_us[(csse_covid_19_daily_reports_us$Long_ < 0 & csse_covid_19_daily_reports_us$Long_ > -130 & !is.na(csse_covid_19_daily_reports_us$Long_)),], aes(Long_, Lat, size = Deaths), fill = nord[[6]],
     shape = 21, alpha = 2/3, stroke = 0.25, color = "#2b2b2b"
   ) +
-  #coord_proj("+proj=wintri") 
   scale_size_area(name = "Deaths", max_size = 20, labels = scales::comma) +
   labs(
     x = NULL, y = NULL,
@@ -79,3 +71,8 @@ ggplot() +
   theme(legend.background = element_rect(fill = nord[[13]], color = nord[[13]]))+
   theme(panel.background = element_rect(fill = nord[[13]], color = nord[[13]]))+
   theme(legend.key = element_rect(fill = nord[[13]], color = nord[[13]]))
+
+
+
+
+# Time Plot
