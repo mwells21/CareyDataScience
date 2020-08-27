ba334f50012b41af4d6267cb2861e91c93904c34
mwells21 <wellsmike19@gmail.com>
2020-08-27 10:01:47 -0400

    shiny framework

---
+++ bookcamp/R/shinyCovid/app.R
@@ -0,0 +1,26 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(readr)
+library(shinydashboard)
+
+# Define UI for application that draws a histogram
+ui <- dashboardPage(
+    dashboardHeader(),
+    dashboardSidebar(),
+    dashboardBody()
+)
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
