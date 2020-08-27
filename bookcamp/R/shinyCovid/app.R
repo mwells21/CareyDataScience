0f16a71782b52dfc680b9c6b84aa931f3c5321a9
mwells21 <wellsmike19@gmail.com>
2020-08-27 11:33:46 -0400

    Update app.R

--- bookcamp/R/shinyCovid/app.R
+++ bookcamp/R/shinyCovid/app.R
@@ -13,13 +13,41 @@ library(shinydashboard)
 
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
-    dashboardHeader(),
-    dashboardSidebar(),
-    dashboardBody()
+    dashboardHeader(title = "Covid-19"),
+    dashboardSidebar(
+        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+        menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    ),
+    dashboardBody(
+        tabItems(
+            # First tab content
+            tabItem(tabName = "dashboard",
+                    fluidRow(
+                        box(plotOutput("plot1", height = 250)),
+                        
+                        box(
+                            title = "Controls",
+                            sliderInput("slider", "Number of observations:", 1, 100, 50)
+                        )
+                    )
+            ),
+            
+            # Second tab content
+            tabItem(tabName = "widgets",
+                    h2("Widgets tab content")
+            )
+        )
+    )
 )
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    set.seed(122)
+    histdata <- rnorm(500)
+    
+    output$plot1 <- renderPlot({
+        data <- histdata[seq_len(input$slider)]
+        hist(data)
+    })
 }
 
 # Run the application 
