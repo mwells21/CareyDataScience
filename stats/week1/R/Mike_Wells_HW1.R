cfb21562d90ca54ff17cb9eb0cbe7ff11877faca
mwells21 <mwells21@uic.edu>
2020-09-10 15:39:09 -0400

    setup for week 1 HW

---
+++ stats/week1/R/Mike_Wells_HW1.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+
+setwd("~/GitHub/CareyDataScience/stats/week1/")
+
+# ---- Load Data ---- 
+CEOdata = read_csv("data/CEOdata.csv")
+conret  = read_csv("data/conret.csv")
+
+
+
+# ---- Problem 1 ----
+
+
+
+
+
