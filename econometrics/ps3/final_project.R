cb536c57c1077432efd7f8cfd71a3e3183d77b45
mwells21 <mwells21@uic.edu>
2020-12-16 16:16:55 -0500

    updates

--- econometrics/ps3/final_project.R
+++ econometrics/ps3/final_project.R
@@ -43,7 +43,7 @@ p + geom_polygon()+
 state_orders$Start = mdy(state_orders$Start)
 state_orders$End = mdy(state_orders$End)
 
-ggplot(data = state_orders, aes(x = State, ymin = Start, ymax = End ))+
+p1 = ggplot(data = state_orders, aes(x = State, ymin = Start, ymax = End ))+
   geom_linerange()+
   geom_point(data = state_orders, aes(x = State, y = Start) )+
   geom_point(data = state_orders, aes(x = State, y = End) )+
@@ -51,19 +51,19 @@ ggplot(data = state_orders, aes(x = State, ymin = Start, ymax = End ))+
   theme(axis.title = element_blank())+
   ggtitle( "Initial Covid Stay-at-Home Order Duration")
 
-ggplot(data = state_orders, aes(x = State, ymin = 0, ymax = Duration ))+
+p2 = ggplot(data = state_orders, aes(x = State, ymin = 0, ymax = Duration ))+
   geom_linerange()+
   geom_point(data = state_orders, aes(x = State, y = 0))+
   geom_point(data = state_orders, aes(x = State, y = Duration) )+
   geom_abline(intercept = min(state_orders$Duration),slope = 0, color = "red")+
   geom_abline(intercept = -min(state_orders$Duration),slope = 0, color = "red")+
   ylim(-30, 300)+
-  theme(axis.title.y = element_blank())+
-  ylab("Days Since Start of Stay-at-Home Order")+
+  theme(axis.title = element_blank())+
   #scale_y_continuous(expand=c(.5,-.5),)+
   coord_flip()+
   ggtitle( "Adjusted Initial Covid Stay-at-Home Order Duration")
 
+plot_grid(p1, p2, labels = "AUTO")
 
 
 
