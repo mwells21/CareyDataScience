397c64a6fb550b361f71dbaee65b1153164c3d58
mwells21 <mwells21@uic.edu>
2020-12-16 16:48:30 -0500

    updates

--- econometrics/ps3/Michael_Wells_PS3.R
+++ econometrics/ps3/Michael_Wells_PS3.R
@@ -28,7 +28,6 @@ brfss2010$age_squared = brfss2010$age^2
 lm2 = lm(sm ~ age + age_squared + binge + black + fem + marry + ban + cigtax + inc + edu, data = brfss2010)
 summary(lm2)
 
-rdplot(brfss2010$sm,dat$age, c=5, p=5) 
 
 
 # Part E ----
@@ -38,7 +37,7 @@ brfss2010$edu_3 = ifelse(brfss2010$edu == 3, 1, 0)
 brfss2010$edu_4 = ifelse(brfss2010$edu == 4, 1, 0)
 
 
-lm3 = lm(sm ~ age + age_squared + binge + black + fem + marry + ban + cigtax + inc + edu_1 + edu_2  + edu_3 + edu_4, data = brfss2010)
+lm3 = lm(sm ~ age + age_squared + binge + black + fem + marry + ban + cigtax + inc + edu_2  + edu_3 + edu_4, data = brfss2010)
 summary(lm3)
 
 
@@ -51,7 +50,7 @@ prev = c(mean(brfss$sm[brfss$year == 2000]), mean(brfss$sm[brfss$year == 2001]),
 plot_df = data.frame(year = unique(brfss$year), prev = prev)
 
 ggplot(data = plot_df, aes(x = year, y = prev))+
-  geom_point(color = "")+
+  geom_point()+
   geom_smooth(method = "lm",formula = y ~ x)
 
 
@@ -59,6 +58,7 @@ ggplot(brfss,
        aes(x = year, 
            fill = as.factor(sm))) + 
   geom_bar(position = "fill") +
+  facet_wrap(~fips)+
   labs(y = "Proportion")
 
 
@@ -72,8 +72,25 @@ brfss$posttreat = brfss$post * brfss$treat
 lm4 = lm(sm ~ post+treat+posttreat,data = brfss)
 summary(lm4)
 
+# Part D -----
+brfss$post_0 = ifelse(brfss$post == 0, 1, 0)
+brfss$post_1 = ifelse(brfss$post == 1, 1, 0)
+brfss$post_2 = ifelse(brfss$post == 2, 1, 0)
+brfss$post_3 = ifelse(brfss$post == 3, 1, 0)
+brfss$post_4 = ifelse(brfss$post == 4, 1, 0)
+brfss$post_5 = ifelse(brfss$post == 5, 1, 0)
 
 
+brfss$posttreat_0 = brfss$post_0 * brfss$treat
+brfss$posttreat_1 = brfss$post_1 * brfss$treat
+brfss$posttreat_2 = brfss$post_2 * brfss$treat
+brfss$posttreat_3 = brfss$post_3 * brfss$treat
+brfss$posttreat_4 = brfss$post_4 * brfss$treat
+brfss$posttreat_5 = brfss$post_5 * brfss$treat
+
+
+lm5 = lm(sm ~ treat + post_1 + post_2 + post_3 + post_4 + post_5 + posttreat_1 + posttreat_2 + posttreat_3 + posttreat_4 + posttreat_5,data = brfss)
+summary(lm5)
 
 
 
