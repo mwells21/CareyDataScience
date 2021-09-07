93a42ac31aca6fa182b44c7f2e5681949b9927e6
mwells21 <wellsmike19@gmail.com>
2021-09-07 13:38:11 -0400

    Comments

--- AI/HelloWorld.R
+++ AI/HelloWorld.R
@@ -3,22 +3,27 @@ library(keras)
 mnist <- dataset_mnist()
 
 
+# ----- Pre Processing -----
+
+# Subset Testing and Training 
 x_train <- mnist$train$x
 y_train <- mnist$train$y
 x_test <- mnist$test$x
 y_test <- mnist$test$y
 
-
+# Reshape
 x_train <- array_reshape(x_train, c(nrow(x_train), 784))
 x_test <- array_reshape(x_test, c(nrow(x_test), 784))
-# rescale
+# Rescale
 x_train <- x_train / 255
 x_test <- x_test / 255
 
-
+# Change Y to cateorical 
 y_train <- to_categorical(y_train, 10)
 y_test <- to_categorical(y_test, 10)
 
+
+# ----- Model ------
 
 model <- keras_model_sequential() 
 model %>% 
